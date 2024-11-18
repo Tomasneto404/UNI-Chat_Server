2d6f132a1b562ac7fc87c1b3368909cea51e9b01
DESKTOP-HUCE4BK\Tomas <tomasneto26@gmail.com>
2024-11-18 15:24:32 +0000

    Resolvido

--- TP1_SD/src/Services/Authorization.java
+++ TP1_SD/src/Services/Authorization.java
@@ -5,27 +5,17 @@ import Enums.OperationType;
 import Enums.Rank;
 
 public class Authorization {
-<<<<<<< HEAD
-    public boolean requestAproval(User user, OperationType operation) {
-        if (operation.equals(OperationType.EVACUATION)) {
-=======
+
     public boolean requestAproval (User user, OperationType operation){
 
         if( operation.equals(OperationType.EVACUATION)){
->>>>>>> 7a552ed (UserInterface)
             return user.getRank().equals(Rank.HIGH);
         }
 
         else if (operation.equals(OperationType.EMERGENCY_COMUNICATION)) {
             return user.getRank().equals(Rank.MEDIUM);
 
-        }
-
-<<<<<<< HEAD
-        else if (operation.equals(OperationType.RESOURCES_DISTRIBUTION)) {
-=======
-        else if(operation.equals(OperationType.RESOURCES_DISTRIBUTION)){
->>>>>>> 7a552ed (UserInterface)
+        } else if(operation.equals(OperationType.RESOURCES_DISTRIBUTION)){
             return user.getRank().equals(Rank.LOW);
         }
 
