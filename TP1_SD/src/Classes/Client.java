c98402b2a1c23d4b5de731b09ec13ca36d0d07ac
DESKTOP-HUCE4BK\Tomas <tomasneto26@gmail.com>
2024-11-22 14:05:42 +0000

    wasd

--- TP1_SD/src/Classes/Client.java
+++ TP1_SD/src/Classes/Client.java
@@ -12,9 +12,19 @@ public class Client {
     private static final String SERVER_ADDRESS = "127.0.0.1";
     private static final int SERVER_PORT = 4444;
 
+    private final String HIGH_MULTICAST_GROUP_ADDRESS = "230.0.0.0";
+    private final int HIGH_MULTICAST_GROUP_PORT = 4445;
+
+    private final String MEDIUM_MULTICAST_GROUP_ADDRESS = "231.0.0.0";
+    private final int MEDIUM_MULTICAST_GROUP_PORT = 4446;
+
+    private final String LOW_MULTICAST_GROUP_ADDRESS = "232.0.0.0";
+    private final int LOW_MULTICAST_GROUP_PORT = 4447;
+
     public static void main(String[] args) {
 
         try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+
              BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
              Scanner scanner = new Scanner(System.in)) {
--- TP1_SD/src/Classes/Server.java
+++ TP1_SD/src/Classes/Server.java
@@ -6,26 +6,31 @@ import java.net.*;
 public class Server {
 
     private final String USERS_FILE = "Users.txt";
-    private final String MESSAGES_FILE = "Messages.txt";
 
-    private final String HIGH_MULTICAST_GROUP = "230.0.0.0";
-    private final String MEDIUM_MULTICAST_GROUP = "231.0.0.0";
-    private final String LOW_MULTICAST_GROUP = "232.0.0.0";
+    private final String HIGH_MULTICAST_GROUP_ADDRESS = "230.0.0.0";
+    private final int HIGH_MULTICAST_GROUP_PORT = 4445;
 
-    private User[] users;
+    private final String MEDIUM_MULTICAST_GROUP_ADDRESS = "231.0.0.0";
+    private final int MEDIUM_MULTICAST_GROUP_PORT = 4446;
 
-    private int port;
+    private final String LOW_MULTICAST_GROUP_ADDRESS = "232.0.0.0";
+    private final int LOW_MULTICAST_GROUP_PORT = 4447;
+
+    private int serverPort;
 
     public Server(int port) {
-        this.port = port;
+        this.serverPort = port;
     }
 
     public void start() throws IOException {
 
         try {
-            ServerSocket ss = new ServerSocket(port);
+            ServerSocket ss = new ServerSocket(serverPort);
 
-            System.out.println("Server connected on port: " + port);
+            MulticastSocket highGroupSocket = new MulticastSocket(HIGH_MULTICAST_GROUP_PORT);
+            highGroupSocket.joinGroup(InetAddress.getByName(HIGH_MULTICAST_GROUP_ADDRESS));
+
+            System.out.println("Server connected on port: " + serverPort);
 
             while (true) {
                 Socket clientSocket = ss.accept();
